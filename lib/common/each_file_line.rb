5b4b90410b89a5323f354c75f17dcc027d83073c
Fiona Bianchi <fiona@hiklas.co.uk>
2016-02-09 21:36:41 +0000

    Completed code/tests for simple file handling class

--- lib/common/each_file_line.rb
+++ lib/common/each_file_line.rb
@@ -10,11 +10,17 @@ module Common
     #
     # Create an instance and hold onto the filename
     #
-
     def initialize(filename)
-
+      @filename = filename
     end
 
+    ##
+    #
+    # Call the block for each line of the file
+    #
+    def each(&block)
+     File.open(@filename).each &block
+    end
 
   end
 
--- test/common/test_each_file_line.rb
+++ test/common/test_each_file_line.rb
@@ -4,6 +4,7 @@ require 'common/each_file_line'
 class TestEachFileLine < Test::Unit::TestCase
 
   TEST_DUMMY_FILENAME = 'TerryPratchett.txt'
+  TEST_REAL_FILENAME = 'test/common/simple_file.txt'
 
   def test_missing_filename
     begin
@@ -19,6 +20,29 @@ class TestEachFileLine < Test::Unit::TestCase
     result = Common::EachFileLine.new(TEST_DUMMY_FILENAME)
     assert(result!=nil, 'The result was nil')
     assert(result.class == Common::EachFileLine, 'Correct class')
+  end
+
+
+  def test_each_file_not_found
+    begin
+      eachFile = Common::EachFileLine.new(TEST_DUMMY_FILENAME)
+      eachFile.each do |line|
+        assert(false, 'This should fail as the file should not exist')
+      end
+
+      assert(false, 'We should not reach this code')
+    rescue Exception => exception
+      assert(exception.class == Errno::ENOENT, "Got exception of type '#{exception.class}'")
+    end
+  end
+
+  def test_each_file
+    counter = 0
+    eachFile = Common::EachFileLine.new(TEST_REAL_FILENAME)
+    eachFile.each do |line|
+      counter+=1
+    end
+    assert(counter==4, "Counter is '#{counter}'")
   end
 
 end
