d7931a9fad1d9b024508e50f28fafa2d41b882aa
Fiona Bianchi <fiona@hiklas.co.uk>
2016-02-10 20:02:06 +0000

    Basic implementation and tests for Q1, code is a little messy though

--- lib/q1/reverse_words.rb
+++ lib/q1/reverse_words.rb
@@ -4,7 +4,18 @@ module Q1
   class ReverseWords
 
     def reverse(input_line)
-      input_line
+      word_array = input_line.split(' ')
+      reversed_string = '';
+
+      word_array.reverse_each do |word|
+        if(reversed_string!='')
+        then
+          reversed_string += ' '
+        end
+        reversed_string += word
+      end
+
+      reversed_string
     end
   end
 
--- spec/q1/reverse_words_spec.rb
+++ spec/q1/reverse_words_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Q1::ReverseWords, '#reverse' do
     end
   end
 
+  context 'with two words' do
+    it 'returns the reverse sentence' do
+      result = @reverser.reverse('One two')
+      expect(result).to eq 'two One'
+    end
+  end
+
+  context 'with three words' do
+    it 'returns the reverse sentence' do
+      result = @reverser.reverse('Pratchett Terry Sir')
+      expect(result).to eq 'Sir Terry Pratchett'
+    end
+  end
+
 end
