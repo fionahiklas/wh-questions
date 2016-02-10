dd954661ffb280177a38165b68e6fa7ce0c3473e
Fiona Bianchi <fiona@hiklas.co.uk>
2016-02-10 20:14:55 +0000

    Refactoring to use Ruby Array methods that can reverse and join, cleaner, more compact code.  Tests still pass, yay :)

--- lib/q1/reverse_words.rb
+++ lib/q1/reverse_words.rb
@@ -7,17 +7,8 @@ module Q1
       # split(' ') works with spaces and tabs, using regular expressions to
       # be more definitive about *exactly* what the line is being split on
       word_array = input_line.split(/[[:blank:]]+/)
-      reversed_string = '';
-
-      word_array.reverse_each do |word|
-        if(reversed_string!='')
-        then
-          reversed_string += ' '
-        end
-        reversed_string += word
-      end
-
-      reversed_string
+      reversed_array = word_array.reverse
+      reversed_array.join(' ')
     end
   end
 
